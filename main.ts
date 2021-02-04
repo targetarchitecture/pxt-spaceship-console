d338799c78262c3b15bd333e175e3d5734bea60c
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2021-02-04 21:55:13 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,53 +1,71 @@
 RainbowSparkleUnicorn.Switch.onSwitchPressed(switchPins.P2, function () {
-    RainbowSparkleUnicorn.Sound.playTrack(1)
+    consoleState = ConsoleStates.YellowAlert;
+RainbowSparkleUnicorn.Sound.playTrack(1)
 })
 input.onButtonPressed(Button.A, function () {
-    rotateLEDs = 1
-    strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    half.setBrightness(255)
-    half.showColor(neopixel.colors(NeoPixelColors.Red))
-    half.show()
+    consoleState = ConsoleStates.VideoPlaying;
 })
 input.onButtonPressed(Button.B, function () {
-    rotateLEDs = 0
-    strip.setBrightness(30)
-    strip.showColor(neopixel.colors(NeoPixelColors.Green))
-    strip.show()
+    consoleState = ConsoleStates.Normal;
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    rotateLEDs = 1
-    strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    half.setBrightness(100)
-    half.showColor(neopixel.colors(NeoPixelColors.Yellow))
-    half.show()
+    consoleState = ConsoleStates.RedAlert;
 })
 let half: neopixel.Strip = null
 let strip: neopixel.Strip = null
 let rotateLEDs = 0
-enum ConsoleStates {Starting, Normal, VideoPlaying, YellowAlert, RedAlert }
+let circularLightTiming = 0
+enum ConsoleStates {Starting , Normal, VideoPlaying, YellowAlert, RedAlert }
 RainbowSparkleUnicorn.start()
 RainbowSparkleUnicorn.printDebugMessages()
 RainbowSparkleUnicorn.printReceivedMessages()
 RainbowSparkleUnicorn.Light.turnAllOn()
 RainbowSparkleUnicorn.Sound.setVolume(10)
 RainbowSparkleUnicorn.Sound.playTrack(2)
-rotateLEDs = 0
-strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
-half = strip.range(0, 10)
-strip.setBrightness(100)
-strip.showColor(neopixel.colors(NeoPixelColors.Green))
-strip.show()
 let consoleState = ConsoleStates.Starting;
 basic.forever(function () {
-    if (rotateLEDs == 1) {
-        strip.rotate(1)
-        strip.show()
-        basic.pause(20)
+    comment.comment("This loop controls the gauge")
+    if (consoleState == ConsoleStates.Normal) {
+        RainbowSparkleUnicorn.Controls.dial1(randint(0, 30))
     } else {
-        basic.pause(1000)
+        RainbowSparkleUnicorn.Controls.dial1(0)
     }
+    basic.pause(2000)
 })
 basic.forever(function () {
-    RainbowSparkleUnicorn.Controls.dial1(randint(0, 30))
-    basic.pause(2000)
+    comment.comment("This loop controls the circular lights")
+    circularLightTiming = 2000
+    if (consoleState == ConsoleStates.Starting) {
+        rotateLEDs = 0
+        strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
+        half = strip.range(0, 10)
+        strip.setBrightness(100)
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+    } else if (consoleState == ConsoleStates.Normal) {
+        strip.setBrightness(30)
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+    } else if (consoleState == ConsoleStates.VideoPlaying) {
+        strip.setBrightness(100)
+        strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
+        strip.show()
+    } else if (consoleState == ConsoleStates.YellowAlert) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
+        half.setBrightness(100)
+        half.showColor(neopixel.colors(NeoPixelColors.Yellow))
+        half.show()
+        strip.rotate(1)
+        circularLightTiming = 50
+    } else if (consoleState == ConsoleStates.RedAlert) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
+        half.setBrightness(255)
+        half.showColor(neopixel.colors(NeoPixelColors.Red))
+        half.show()
+        strip.rotate(1)
+        circularLightTiming = 20
+    } else {
+        comment.comment("Should never have no state")
+    }
+    basic.pause(circularLightTiming)
 })
