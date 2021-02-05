32ca6605b5ca29b92b6ab21ec4dcb2082b431f25
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2021-02-05 00:11:39 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,37 @@
 RainbowSparkleUnicorn.Switch.onSwitchPressed(switchPins.P2, function () {
-    consoleState = ConsoleStates.YellowAlert;
-RainbowSparkleUnicorn.Sound.playTrack(1)
+    consoleState = 3
+    RainbowSparkleUnicorn.Sound.playTrack(1)
 })
 input.onButtonPressed(Button.A, function () {
-    consoleState = ConsoleStates.VideoPlaying;
+    consoleState = 2
 })
 input.onButtonPressed(Button.B, function () {
-    consoleState = ConsoleStates.Normal;
+    consoleState = 1
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    consoleState = ConsoleStates.RedAlert;
+    consoleState = 4
 })
-let half: neopixel.Strip = null
-let strip: neopixel.Strip = null
-let rotateLEDs = 0
-let circularLightTiming = 0
-enum ConsoleStates {Starting , Normal, VideoPlaying, YellowAlert, RedAlert }
+/**
+ * enum ConsoleStates {Starting =0  , Normal=1, VideoPlaying=2, YellowAlert=3, RedAlert=4 }
+ */
+/**
+ * let circularLightLoopPauseMs = 0
+ */
+let stateInCircularLightLoop = 0
+let circularLightLoopPauseMs = 0
+let consoleState = 0
 RainbowSparkleUnicorn.start()
 RainbowSparkleUnicorn.printDebugMessages()
 RainbowSparkleUnicorn.printReceivedMessages()
 RainbowSparkleUnicorn.Light.turnAllOn()
 RainbowSparkleUnicorn.Sound.setVolume(10)
 RainbowSparkleUnicorn.Sound.playTrack(2)
-let consoleState = ConsoleStates.Starting;
+let strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
+let half = strip.range(0, 10)
+consoleState = 1
 basic.forever(function () {
     comment.comment("This loop controls the gauge")
-    if (consoleState == ConsoleStates.Normal) {
+    if (consoleState == 1) {
         RainbowSparkleUnicorn.Controls.dial1(randint(0, 30))
     } else {
         RainbowSparkleUnicorn.Controls.dial1(0)
@@ -34,43 +40,58 @@ basic.forever(function () {
 })
 basic.forever(function () {
     comment.comment("This loop controls the circular lights")
-    circularLightTiming = 500
-    if (consoleState == ConsoleStates.Starting) {
-        rotateLEDs = 0
-        strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
-        half = strip.range(0, 10)
-        strip.setBrightness(100)
-        strip.showColor(neopixel.colors(NeoPixelColors.Green))
-        strip.show()
-    } else if (consoleState == ConsoleStates.Normal) {
-        rotateLEDs = 0
-        strip.setBrightness(30)
-        strip.showColor(neopixel.colors(NeoPixelColors.Green))
-        strip.show()
-    } else if (consoleState == ConsoleStates.VideoPlaying) {
-        rotateLEDs = 0
-        strip.setBrightness(100)
-        strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
-        strip.show()
-    } else if (consoleState == ConsoleStates.YellowAlert) {
-        if (rotateLEDs == 1) {
+    circularLightLoopPauseMs = 1000
+    comment.comment("Need to check for a transition, so we can set up the lights")
+    if (stateInCircularLightLoop != consoleState) {
+        if (consoleState == 1) {
+            serial.writeLine("1.1")
+            strip.setBrightness(30)
+            strip.showColor(neopixel.colors(NeoPixelColors.Green))
+            strip.show()
+        } else if (consoleState == 2) {
+            serial.writeLine("2.1")
+            strip.setBrightness(100)
+            strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
+            strip.show()
+        } else if (consoleState == 3) {
+            serial.writeLine("3.1")
             strip.showColor(neopixel.colors(NeoPixelColors.Black))
             half.setBrightness(100)
             half.showColor(neopixel.colors(NeoPixelColors.Yellow))
             half.show()
+        } else if (consoleState == 4) {
+            serial.writeLine("4.1")
+            strip.showColor(neopixel.colors(NeoPixelColors.Black))
+            half.setBrightness(255)
+            half.showColor(neopixel.colors(NeoPixelColors.Red))
+            half.show()
+        } else {
+            comment.comment("Should never have no state")
         }
+    }
+    if (stateInCircularLightLoop == 0) {
+        serial.writeLine("0")
+        comment.comment("This loop is fairly fixed to setup green")
+    } else if (stateInCircularLightLoop == 1) {
+        serial.writeLine("1")
+    } else if (stateInCircularLightLoop == 2) {
+        serial.writeLine("2")
+    } else if (stateInCircularLightLoop == 3) {
+        comment.comment("Need to check for a transition, so we can set up the light")
+        serial.writeLine("3")
+        comment.comment("Just spin the light and reduce loop speed")
         strip.rotate(1)
-        rotateLEDs = 0
-        circularLightTiming = 50
-    } else if (consoleState == ConsoleStates.RedAlert) {
-        strip.showColor(neopixel.colors(NeoPixelColors.Black))
-        half.setBrightness(255)
-        half.showColor(neopixel.colors(NeoPixelColors.Red))
-        half.show()
+        strip.show()
+        circularLightLoopPauseMs = 50
+    } else if (stateInCircularLightLoop == 4) {
+        serial.writeLine("4")
         strip.rotate(1)
-        circularLightTiming = 20
+        strip.show()
+        circularLightLoopPauseMs = 20
     } else {
         comment.comment("Should never have no state")
     }
-    basic.pause(circularLightTiming)
+    // serial.writeLine("stateInCircularLightLoop: " + stateInCircularLightLoop + " consoleState: " + consoleState)
+    stateInCircularLightLoop = consoleState
+    basic.pause(circularLightLoopPauseMs)
 })
