b2912ea6d888acaefb382b93ac070f7db37696e6
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2021-02-10 08:19:37 +0000

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -79,6 +79,8 @@ RainbowSparkleUnicorn.printReceivedMessages()
 RainbowSparkleUnicorn.Light.turnAllOff()
 // This is the big red button
 RainbowSparkleUnicorn.Light.turnOn(lightPins.P15)
+sortOutFuelLights();
+
 RainbowSparkleUnicorn.Sound.setVolume(2)
 RainbowSparkleUnicorn.Sound.playTrack(2)
 let horizonLevelAngle = 110
@@ -166,4 +168,28 @@ basic.forever(function () {
     stateInCircularLightLoop = consoleState
     comment.comment("pause for how long...")
     basic.pause(circularLightLoopPauseMs)
+})
+
+RainbowSparkleUnicorn.Switch.onSwitchReleased(switchPins.P4, function () {
+    sortOutFuelLights()
+})
+RainbowSparkleUnicorn.Switch.onSwitchPressed(switchPins.P5, function () {
+    sortOutFuelLights()
+})
+RainbowSparkleUnicorn.Switch.onSwitchPressed(switchPins.P4, function () {
+    sortOutFuelLights()
+})
+function sortOutFuelLights () {
+    basic.clearScreen()
+    RainbowSparkleUnicorn.Light.turnOff(lightPins.P9)
+    RainbowSparkleUnicorn.Light.turnOff(lightPins.P8)
+    if (RainbowSparkleUnicorn.Switch.getSwitchState(switchPins.P4) == 1) {
+        RainbowSparkleUnicorn.Light.turnOn(lightPins.P9)
+    }
+    if (RainbowSparkleUnicorn.Switch.getSwitchState(switchPins.P5) == 1) {
+        RainbowSparkleUnicorn.Light.turnOn(lightPins.P8)
+    }
+}
+RainbowSparkleUnicorn.Switch.onSwitchReleased(switchPins.P5, function () {
+    sortOutFuelLights()
 })
