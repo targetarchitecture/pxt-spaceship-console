0025a9805d88bc5f9cad2921873eff502840d3ae
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2021-10-11 00:02:06 +0100

    Update pxt.json, main.ts, volume.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,4 @@
 
-let BB = "----------------"
-
 startUp();
 
 basic.forever(function () {
@@ -27,39 +25,10 @@ basic.forever(function () {
     sortOutFuelLights();
     basic.pause(500)
 })
-basic.forever(function () {
-
-    let AA = RainbowSparkleUnicorn._readMessage("SUPDATE");
-
-    if (AA.compare(BB)!=0  ) {
-        serial.writeLine("switchStates:" + AA + " previousSwitchStates:" + BB);
-    }
-
-    // for (let index = 0; index < 16; index++) {
-
-    //     const pinState = switchStates.charAt(index);
-    //     const previousPinState = previousSwitchStates.charAt(index);
-
-    //     if (pinState != previousPinState) {
-
-    //         serial.writeLine("index: " + index + " pinState: " + pinState + " switchStates:" + switchStates + " previousPinState:" + previousPinState);
-
-    //         //     if (pinState == "L") {
-    //         //         control.raiseEvent(RAINBOW_SPARKLE_UNICORN_SWITCH_PRESSED, index + 1);
-    //         //     } else if (pinState == "H") {
-    //         //         control.raiseEvent(RAINBOW_SPARKLE_UNICORN_SWITCH_RELEASED, index + 1);
-    //         //     }
-    //     }
-    // }
-
-    BB = AA;
-    //}
-
-
-    // touchStatus = RainbowSparkleUnicorn.Touch.getTouchStates()
-    //switchStatus = RainbowSparkleUnicorn.Switch.getSwitchStates()
-    // serial.writeLine(switchStatus);
-    basic.pause(250); //50
+basic.forever(function () {    
+    switchStatus = RainbowSparkleUnicorn.Switch.getSwitchStates()
+    serial.writeLine("SS:" + switchStatus);
+    basic.pause(100); 
 })
 basic.forever(function () {
     RainbowSparkleUnicorn.comment("This loop controls the circular lights")
--- volume.ts
+++ volume.ts
@@ -7,27 +7,6 @@ function volumeControl() {
     if (volumeMapped != tempVol) {
         volumeMapped = tempVol;
         RainbowSparkleUnicorn.Sound.setVolume(volumeMapped);
-        //serial.writeValue("vol", volumeMapped);
     }
 }
 
-// let slider = 0
-// let volume = 0
-// RainbowSparkleUnicorn.start()
-// RainbowSparkleUnicorn.Controls.dial1(128)
-// RainbowSparkleUnicorn.Controls.dial2(128)
-// basic.forever(function () {
-//     let volumeMapped = 0
-//     volume = 1023 - pins.analogReadPin(AnalogPin.P0)
-//     slider = pins.analogReadPin(AnalogPin.P2)
-//     // led.plotBarGraph(
-//     // slider,
-//     // 1023
-//     // )
-//     serial.writeValue("v", volume)
-//     // serial.writeValue("s", slider)
-//     volumeMapped = Math.round(Math.map(volume, 0, 1023, 0, 30))
-//     // basic.showNumber(volumeMapped)
-//     serial.writeValue("vol", volumeMapped)
-//     basic.pause(100);
-// })
