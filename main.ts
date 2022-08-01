42e959a7a24d2e654abdbef5526994032a374d07
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2022-08-01 13:24:11 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,50 +1,57 @@
-function start () {
+function start() {
     consoleState = "Starting"
     basic.showIcon(IconNames.Heart)
     RainbowSparkleUnicorn.start()
     RainbowSparkleUnicorn.Light.turnAllOn()
     RainbowSparkleUnicorn.Sound.stop()
+
+    volumeControl();
+
     control.inBackground(() => {
         while (true) {
             volumeControl();
             basic.pause(250)
         }
     })
-RainbowSparkleUnicorn.Light.turnAllOff()
+
+    RainbowSparkleUnicorn.Light.turnAllOff()
     control.inBackground(() => {
         while (true) {
             ringLights();
             basic.pause(20)
         }
     })
-RainbowSparkleUnicorn.Sound.playTrack(Math.randomRange(1, 2))
+
+    RainbowSparkleUnicorn.Sound.playTrack(Math.randomRange(1, 2))
+
     RainbowSparkleUnicorn.Switch.RequestSwitchStates();
-control.inBackground(() => {
+
+    control.inBackground(() => {
         while (true) {
             artificialHorizon();
             basic.pause(20)
         }
     })
-RainbowSparkleUnicorn.Light.turnOn(RainbowSparkleUnicorn.Light.Pins.P0)
+    RainbowSparkleUnicorn.Light.turnOn(RainbowSparkleUnicorn.Light.Pins.P0)
     control.inBackground(() => {
         while (true) {
             pressureGauge();
             basic.pause(1000)
         }
     })
-RainbowSparkleUnicorn.Light.breathe(
-    RainbowSparkleUnicorn.Light.Pins.P13,
-    1000,
-    500,
-    250,
-    250
+    RainbowSparkleUnicorn.Light.breathe(
+        RainbowSparkleUnicorn.Light.Pins.P13,
+        1000,
+        500,
+        250,
+        250
     )
     RainbowSparkleUnicorn.Light.breathe(
-    RainbowSparkleUnicorn.Light.Pins.P14,
-    1000,
-    500,
-    250,
-    250
+        RainbowSparkleUnicorn.Light.Pins.P14,
+        1000,
+        500,
+        250,
+        250
     )
     basic.showIcon(IconNames.SmallHeart)
     consoleState = "Normal"
